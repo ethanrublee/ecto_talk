fc5c9709e9a3fe880e68c26b8115d569b2f49a29
Troy Straszheim <straszheim@willowgarage.com>
2011-08-30 11:13:02 -0700

    more kinectness

---
+++ kinect_voxelgrid.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+"""
+This example shows how to capture a Kinect point
+cloud and display it to a cloud viewer.
+"""
+
+import ecto, ecto_openni
+from ecto_pcl import *
+
+plasm = ecto.Plasm()
+
+cap = ecto_openni.Capture()
+conv = NiConverter()
+
+voxgrid = VoxelGrid("VoxelGrid", leaf_size=0.02)
+
+viewer = CloudViewer("Viewer", window_name="Clouds!")
+
+plasm.connect(cap[:] >> conv[:],
+              conv[:] >> voxgrid[:],
+              voxgrid[:] >> viewer[:])
+
+if __name__ == "__main__":
+    sched = ecto.schedulers.Threadpool(plasm)
+    sched.execute()
+
+
+
+
